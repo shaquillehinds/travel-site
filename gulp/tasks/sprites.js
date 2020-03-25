a33aacfde3729f4abc596e08f5f8bf45af06272a
Shaq <shaqdulove@hotmail.com>
2020-03-25 19:21:29 -0400

    created sprites

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp"),
+  svgSprite = require("gulp-svg-sprite");
+
+let config = {
+  mode: {
+    css: {
+      render: {
+        css: {
+          template: "./gulp/templates/sprite.css"
+        }
+      }
+    }
+  }
+};
+
+gulp.task("createSprite", () => {
+  return gulp
+    .src("./app/assets/images/icons/**/*.svg")
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest("./app/temp/sprite/"));
+});
