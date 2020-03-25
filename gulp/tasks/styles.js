8da3b606b3853ffe2926f00d3e3d4d2aac2a79dd
Shaq <shaqdulove@hotmail.com>
2020-03-25 12:54:53 -0400

    completed intro content block

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ const gulp = require("gulp"),
   autoprefixer = require("autoprefixer"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
+  mixins = require("postcss-mixins"),
   cssImport = require("postcss-import");
 
 let styles = gulp.task("styles", () => {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, nested, autoprefixer, cssvars]))
+    .pipe(postcss([cssImport, mixins, nested, autoprefixer, cssvars]))
     .on("error", function(err) {
       console.log(err.toString());
       this.emit("end");
