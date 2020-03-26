080e2cc934b00d48f56cb841c67fa88405f6b315
Shaq <shaqdulove@hotmail.com>
2020-03-25 20:10:57 -0400

    sprite gen completed

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp"),
-  svgSprite = require("gulp-svg-sprite");
+  svgSprite = require("gulp-svg-sprite"),
+  rename = require("gulp-rename"),
+  del = require("del");
 
 let config = {
   mode: {
     css: {
+      sprite: "sprite.svg",
       render: {
         css: {
           template: "./gulp/templates/sprite.css"
@@ -13,9 +16,38 @@ let config = {
   }
 };
 
-gulp.task("createSprite", () => {
+gulp.task("beginClean", () => {
+  return del(["./app/temp/sprite", "./app/assets/images/sprites"]);
+});
+
+gulp.task("createSprite", ["beginClean"], () => {
   return gulp
     .src("./app/assets/images/icons/**/*.svg")
     .pipe(svgSprite(config))
     .pipe(gulp.dest("./app/temp/sprite/"));
 });
+
+gulp.task("copySpriteGraphic", ["createSprite"], () => {
+  return gulp
+    .src("./app/temp/sprite/css/**/*.svg")
+    .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprite"], () => {
+  return gulp
+    .src("./app/temp/sprite/css/*.css")
+    .pipe(rename("_sprite.css"))
+    .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("endClean", ["copySpriteGraphic", "copySpriteCSS"], () => {
+  return del("./app/temp/sprite");
+});
+
+gulp.task("icons", [
+  "beginClean",
+  "createSprite",
+  "copySpriteGraphic",
+  "copySpriteCSS",
+  "endClean"
+]);
